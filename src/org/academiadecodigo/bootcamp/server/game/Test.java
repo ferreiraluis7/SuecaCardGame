6703c9ad4628a4fb5411b9de2430ff115b7c6f5a
CesarCota <cesarmiguelcota@gmail.com>
2017-12-24 00:17:27 -0100

    Created an enum Cards with unicode set

---
+++ src/org/academiadecodigo/bootcamp/server/game/Test.java
@@ -0,0 +1,12 @@
+package org.academiadecodigo.bootcamp.server.game;
+
+public class Test {
+    public static void main(String[] args) {
+        for (Cards cards : Cards.values()) {
+            System.out.println(cards.getUnicode());
+            System.out.println(cards.getUnicode2());
+        }
+
+
+    }
+}
